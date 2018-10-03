42bdc721a70e6b873183d29531f7966e1a0d2582
Pratik521992 <pratik521992@gmail.com>
2018-10-03 10:21:04 +0530

    Updates

--- parallax.js
+++ parallax.js
@@ -5,6 +5,29 @@ $(window).scroll(function(){
 function parallax(){
     var wscr = $(window).scrollTop();
     
-    $('.section').css('top', (wscr*0.5)+ 'px')
+    $('.section2text').css({
+
+        'transform' :  'translate( 0px, '+  wscr/90+ 'rem)'
+        
+    });
+
+  
+    
+   
+    /*
+    $('.section2knife').css({
+
+        'transform' :  'rotate(' +  - wscr/10 + 'deg)',
+        
+        
+    });*/
+    if(wscr > $('.pig3').offset().top /*-( $(window).height() /1.5 )*/){
+        
+        $('.pig3text').addClass('is-showing');
+    }
+    if(wscr > $('.section-game').offset().top /*-( $(window).height() /1.5 )*/){
+        $('.game').addClass('.go');
+}
+
 
 }
